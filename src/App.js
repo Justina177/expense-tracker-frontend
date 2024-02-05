17a7b203477b03bff1a34570d9cdccf92dc83ebe
justina177 <justinaenenche@yahoo.com>
2024-02-05 04:50:49 +0100

    Added keyframe animation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import styled from "styled-components";
-import bg from './img/bg.jpeg'
+import bg2 from './img/bg.jpeg'
+import { MainLayout } from "./styles/Layouts";
+import Orb from "./Components/Orb/Orb";
 
 function App() {
   return (
-    <AppStyled bg={bg} className="App">
-      
+    <AppStyled bg={bg2} className="App">
+      <Orb />
+      <MainLayout>
+        <h1>Hello</h1>
+
+      </MainLayout>
     </AppStyled>
   );
 }
 
 const AppStyled = styled.div`
 height: 100vh;
-background-image: url(${props => props.bg});
+background-image: url(${props => props.bg2});
 position: relative;
 `
 
